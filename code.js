a0cbb24654e4bd283ba1ff7a9dad6a68e1857d61
Abu Bakar <test@gmail.com>
2020-12-26 11:46:56 -0800

    Even Odd Program at its right Number

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
-var num=prompt("Enter the number of the First Subject")
-var num1=prompt("Enter the number of the Second Subject")
-var num2=prompt("Enter the number of the Third Subject")
-var avg= (num+num1+num2)/3
-if (avg>=80) {
-	alert("You are Above Standard")
+var number=prompt("Enter Your Number")
+if (number%2==1) {
+	alert("Your Number Is Odd")
+}
+else{
+	alert("Your Number Is Even")
 }
