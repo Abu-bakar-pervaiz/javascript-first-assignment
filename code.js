710268c40c01e6faba169199dded3df0df280109
Abu Bakar <test@gmail.com>
2020-12-26 11:39:38 -0800

    Modification in length Converter w.r.t Syllabus

--- code.js
+++ code.js
@@ -1,5 +1,3 @@
-function inch_to_cm_converter() {
-	var I_len=document.getElementById('input').value
-	var C_len=(2.54*I_len)
-	document.getElementById('results').innerHTML="Your Length In Centimeters is :" + parseFloat(C_len) 
-}
+var I_len=prompt("Enter Length in Inches")
+var C_len=(2.54*I_len)
+console.log("Your Length in Centimeters is : " + C_len)
