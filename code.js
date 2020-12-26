b7197f1c6d21b92f309cd2e2edbeeb38b4fd4edf
Abu Bakar <test@gmail.com>
2020-12-26 11:36:55 -0800

    Some modification in the code w.r.t to the Syllabus

--- code.js
+++ code.js
@@ -1,5 +1,3 @@
-function temp_converter() {
-	var C_temp=document.getElementById('input').value
-	var F_temp=32+(9/5)*C_temp
-	document.getElementById('results').innerHTML="Your Temperature In Fahrenheit is :" + parseInt(F_temp) 
-}
+var C_temp=prompt("Enter Your Temperature in Celius")
+var F_temp=32+(9/5)*C_temp
+alert("Your Temperature in Fahrenheit Is : " + F_temp)	
